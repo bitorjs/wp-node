6e5a9c05730f784832aea7bd544b8b6ab2ce26d9
huangzj <huangzj@mzftech.cn>
2019-03-06 10:27:16 +0800

    f

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -2,10 +2,12 @@ const webpack = require('webpack')
 const WebpackMerge = require('webpack-merge');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 const base = require('./webpack.base');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin')
 
 module.exports = WebpackMerge(base, {
   mode: 'development',
   plugins: [
+    new OpenBrowserPlugin({ url: 'http://localhost:8044' }),
     /* HMR plugin */
     new webpack.HotModuleReplacementPlugin(),
     /* 当 HMR 替换时在浏览器控制台输出对用户更友好的模块名字信息 */
